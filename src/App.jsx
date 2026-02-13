4512a46de77feaf2c90264dfde86ecf3ca2c373f
HBhudiya <h.bhudiya@outlook.com>
2026-02-13 15:29:55 +0000

    Activity 02 - Creating a Page Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,38 @@
+import "./App.scss";
+
 function App() {
-  return <p>Hello World!</p>;
+  const loggedInUser = "John";
+
+  return (
+    <div className="layout">
+      <header>
+        <h1>Basic React Demo</h1>
+        <p className="welcome">Welcome {loggedInUser}</p>
+      </header>
+
+      <nav>
+        <div className="navItem">
+          <a to="/">Home</a>
+        </div>
+
+        <div className="navItem">
+          <a to="/">Modules</a>
+        </div>
+
+        <div className="navItem">
+          <a to="/">Students</a>
+        </div>
+      </nav>
+
+      <main>
+        <p>Home Page</p>
+      </main>
+
+      <footer>
+        <p className="thankyou">Thank You For Using This System!</p>
+      </footer>
+    </div>
+  );
 }
 
 export default App;

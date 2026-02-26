70b9fbcc7e6db252426106150646e5fa66711ce1
HBhudiya <h.bhudiya@outlook.com>
2026-02-26 16:05:33 +0000

    Activity 23 - Refactoring a Reusable Form Component

--- src/components/entity/module/ModuleForm.jsx
+++ src/components/entity/module/ModuleForm.jsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import useLoad from "../../api/useLoad.js";
 import apiURL from "../../api/apiURL.js";
-import { Confirm, useAlert } from "../../UI/Alert.jsx";
-import Spacer from "../../UI/Spacer.jsx";
-import Action from "../../UI/Actions.jsx";
+import Form from "../../UI/Form.jsx";
 import "./ModuleForm.scss";
 
 const initialModule = {
@@ -42,134 +39,109 @@ const ModuleForm = ({ onSubmit, onCancel }) => {
   const staffEndpoint = `${apiURL}/users/staff`;
 
   // State
-  const [module, setModule] = useState(initialModule);
+  const [module, handleChange, handleSubmit] = Form.useForm(
+    initialModule,
+    conformance,
+    onSubmit,
+  );
   const [years, loadingYearsMessage] = useLoad(yearsEndpoint);
   const [staff, loadingStaffMessage] = useLoad(staffEndpoint);
-  const [isConfirmOpen, confirmMessage, openConfirm, closeConfirm] = useAlert();
 
   // Handlers
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setModule({ ...module, [name]: conformance.html2js[name](value) });
-  };
-
-  const handleSubmit = () => onSubmit(module);
 
   // View
   return (
-    <div className="moduleForm">
-      {isConfirmOpen && (
-        <Confirm
-          message={confirmMessage}
-          onDismiss={closeConfirm}
-          onConfirm={handleSubmit}
+    <Form onSubmit={handleSubmit} onCancel={onCancel}>
+      <label>
+        Module Name
+        <input
+          type="text"
+          name="ModuleName"
+          value={conformance.js2html.ModuleName(module.ModuleName)}
+          onChange={handleChange}
         />
-      )}
+      </label>
 
-      <Spacer>
-        <div className="FormTray">
-          <label>
-            Module Name
-            <input
-              type="text"
-              name="ModuleName"
-              value={conformance.js2html.ModuleName(module.ModuleName)}
-              onChange={handleChange}
-            />
-          </label>
+      <label>
+        Module Code
+        <input
+          type="text"
+          name="ModuleCode"
+          value={conformance.js2html.ModuleCode(module.ModuleCode)}
+          onChange={handleChange}
+        />
+      </label>
 
-          <label>
-            Module Code
-            <input
-              type="text"
-              name="ModuleCode"
-              value={conformance.js2html.ModuleCode(module.ModuleCode)}
-              onChange={handleChange}
-            />
-          </label>
+      <label>
+        Module Level
+        <select
+          name="ModuleLevel"
+          value={conformance.js2html.ModuleLevel(module.ModuleLevel)}
+          onChange={handleChange}
+        >
+          <option value="0" hidden>
+            No level Selected
+          </option>
+          {[3, 4, 5, 6, 7].map((level) => (
+            <option key={level}>{level}</option>
+          ))}
+        </select>
+      </label>
 
-          <label>
-            Module Level
-            <select
-              name="ModuleLevel"
-              value={conformance.js2html.ModuleLevel(module.ModuleLevel)}
-              onChange={handleChange}
-            >
-              <option value="0" hidden>
-                No level Selected
+      <label>
+        Module Year
+        {!years ? (
+          <p>{loadingYearsMessage}</p>
+        ) : (
+          <select
+            name="ModuleYearID"
+            value={conformance.js2html.ModuleYearID(module.ModuleYearID)}
+            onChange={handleChange}
+          >
+            <option value="0" hidden>
+              No Year Selected
+            </option>
+            {years.map((year) => (
+              <option key={year.YearID} value={year.YearID}>
+                {year.YearName}
               </option>
-              {[3, 4, 5, 6, 7].map((level) => (
-                <option key={level}>{level}</option>
-              ))}
-            </select>
-          </label>
+            ))}
+          </select>
+        )}
+      </label>
 
-          <label>
-            Module Year
-            {!years ? (
-              <p>{loadingYearsMessage}</p>
-            ) : (
-              <select
-                name="ModuleYearID"
-                value={conformance.js2html.ModuleYearID(module.ModuleYearID)}
-                onChange={handleChange}
-              >
-                <option value="0" hidden>
-                  No Year Selected
-                </option>
-                {years.map((year) => (
-                  <option key={year.YearID} value={year.YearID}>
-                    {year.YearName}
-                  </option>
-                ))}
-              </select>
-            )}
-          </label>
+      <label>
+        Module Leader
+        {!staff ? (
+          <p>{loadingStaffMessage}</p>
+        ) : (
+          <select
+            name="ModuleLeaderID"
+            value={conformance.js2html.ModuleLeaderID(module.ModuleLeaderID)}
+            onChange={handleChange}
+          >
+            <option value="0" hidden>
+              No User Selected
+            </option>
+            {staff.map((user) => (
+              <option key={user.UserID} value={user.UserID}>
+                {`${user.UserFirstname} ${user.UserLastname}`}
+              </option>
+            ))}
+          </select>
+        )}
+      </label>
 
-          <label>
-            Module Leader
-            {!staff ? (
-              <p>{loadingStaffMessage}</p>
-            ) : (
-              <select
-                name="ModuleLeaderID"
-                value={conformance.js2html.ModuleLeaderID(
-                  module.ModuleLeaderID,
-                )}
-                onChange={handleChange}
-              >
-                <option value="0" hidden>
-                  No User Selected
-                </option>
-                {staff.map((user) => (
-                  <option key={user.UserID} value={user.UserID}>
-                    {`${user.UserFirstname} ${user.UserLastname}`}
-                  </option>
-                ))}
-              </select>
-            )}
-          </label>
-
-          <label>
-            Module Image
-            <input
-              type="text"
-              name="ModuleImageURL"
-              value={conformance.js2html.ModuleImageURL(module.ModuleImageURL)}
-              onChange={handleChange}
-            />
-          </label>
-        </div>
-
-        <Action.Tray>
-          <Action.Submit
-            showText
-            onClick={() => openConfirm("Are You Sure You Want To Submit?")}
-          />
-          <Action.Cancel showText buttonText="Cancel Form" onClick={onCancel} />
-        </Action.Tray>
-      </Spacer>
-    </div>
+      <label>
+        Module Image
+        <input
+          type="text"
+          name="ModuleImageURL"
+          value={conformance.js2html.ModuleImageURL(module.ModuleImageURL)}
+          onChange={handleChange}
+        />
+      </label>
+    </Form>
   );
 };
 
